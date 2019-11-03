a5c87f1438e3cafc06a05f3f9a6fa856ce53b8b8
lderoeck <lena.deroeck@lucedaphotonics.com>
2019-11-03 14:03:10 +0100

    Adding headers

--- src/IGMP.hh
+++ src/IGMP.hh
@@ -2,7 +2,7 @@
 #define CLICKNET_IGMP_H
 #include <clicknet/ip.h>
 
-struct click_igmp {
+struct IGMP_groupmessage {
     uint8_t	igmp_type;	// 0x11	
     uint8_t	igmp_code;		
     uint16_t	igmp_cksum;		
@@ -15,7 +15,28 @@ struct click_igmp {
     };
     uint8_t igmp_qqic;
     uint16_t igmp_n;
-    struct in_addr igmp_source;
+    struct in_addr[igmp_n] igmp_sources;
 };
+
+struct IGMP_reportmessage
+{
+    uint8_t	igmp_type;	// 0x22	
+    uint8_t	igmp_resv1;		
+    uint16_t	igmp_cksum;
+    uint16_t    igmp_resv2;
+    uint16_t    igmp_n;
+    struct IGMP_grouprecord[igmp_n] igmp_grouprecords;
+};
+
+struct IGMP_grouprecord
+{
+    uint8_t	igmp_type;	
+    uint8_t	igmp_auxdlen;		
+    uint16_t	igmp_n;
+    uint32_t    igmp_groupadress;
+    struct in_addr[igmp_n] igmp_sources;
+    //aux data
+};
+
 
 #endif
