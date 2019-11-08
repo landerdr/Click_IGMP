0e6473c36be060982b5aa1742986554181d25eda
QuentinDeHaes <Quentin.DeHaes@student.uantwerpen.be>
2019-11-08 18:16:08 +0100

    commit

--- src/IGMP.hh
+++ src/IGMP.hh
@@ -3,32 +3,21 @@
 #include <clicknet/ip.h>
 #include <click/vector.cc>
 
+
 struct IGMP_groupmessage
 {
     uint8_t	igmp_type;	// 0x11	
     uint8_t	igmp_code;		
     uint16_t	igmp_cksum;		
     uint32_t	igmp_groupadress;
-    struct igmp_c
-    {
-        unsigned int igmp_resv : 4;
-        unsigned int igmp_s : 1;
-        unsigned int igmp_qrv: 3;
-    };
+    unsigned int igmp_resv : 4;
+    unsigned int igmp_s : 1;
+    unsigned int igmp_qrv: 3;
     uint8_t igmp_qqic;
     uint16_t igmp_n;
     Vector<in_addr> igmp_sources;
 };
 
-struct IGMP_reportmessage
-{
-    uint8_t	igmp_type;	// 0x22	
-    uint8_t	igmp_resv1;		
-    uint16_t	igmp_cksum;
-    uint16_t    igmp_resv2;
-    uint16_t    igmp_n;
-    Vector<IGMP_grouprecord> igmp_grouprecords;
-};
 
 struct IGMP_grouprecord
 {
@@ -39,6 +28,16 @@ struct IGMP_grouprecord
     Vector<in_addr> igmp_sources;
     //aux data
 };
+struct IGMP_reportmessage
+{
+    uint8_t	igmp_type;	// 0x22
+    uint8_t	igmp_resv1;
+    uint16_t	igmp_cksum;
+    uint16_t    igmp_resv2;
+    uint16_t    igmp_n;
+    Vector<IGMP_grouprecord> igmp_grouprecords;
+};
+
 
 
 #endif
