b59400c3ed3023c1095a0e4fb849799472c89397
LanderDeRoeck <Lander.DeRoeck@student.uantwerpen.be>
2019-12-15 18:43:09 +0100

    Fixing same packet pointer issue

--- src/report_sender.hh
+++ src/report_sender.hh
@@ -1,5 +1,6 @@
 #ifndef CLICK_report_sender_HH
 #define CLICK_report_sender_HH
+
 #include <click/element.hh>
 #include <click/timer.hh>
 #include <click/vector.cc>
@@ -8,31 +9,39 @@
 
 CLICK_DECLS
 
-class report_sender : public Element
-{
+class report_sender : public Element {
 public:
-	report_sender();
-	~report_sender();
+    report_sender();
 
-	const char *class_name() const { return "report_sender"; }
-	const char *port_count() const { return "1/1"; }
-	const char *processing() const { return PUSH; }
-	int configure(Vector<String> &, ErrorHandler *);
+    ~report_sender();
 
-	void push(int interface, Packet *p);
-	Vector<Group *> groups;
-	Vector<clientTimer> timers;
-	IPAddress src;
-	void run_timer(Timer *t);
-	Vector<packetTimer> mode_changes;
+    const char *class_name() const { return "report_sender"; }
+
+    const char *port_count() const { return "1/1"; }
+
+    const char *processing() const { return PUSH; }
+
+    int configure(Vector <String> &, ErrorHandler *);
+
+    void push(int interface, Packet *p);
+
+    Vector<Group *> groups;
+    Vector <clientTimer> timers;
+    IPAddress src;
+
+    void run_timer(Timer *t);
+
+    Vector <packetTimer> mode_changes;
 
 private:
-	static int join_group(const String &conf, Element *e, void *thunk, ErrorHandler *errh);
-	static int leave_group(const String &conf, Element *e, void *thunk, ErrorHandler *errh);
-	void add_handlers();
+    static int join_group(const String &conf, Element *e, void *thunk, ErrorHandler *errh);
 
-	IPAddress dst;
-	Timer timer;
+    static int leave_group(const String &conf, Element *e, void *thunk, ErrorHandler *errh);
+
+    void add_handlers();
+
+    IPAddress dst;
+    Timer timer;
     unsigned max_resp_time = 100; // maximum response time | default = 100 (10s)
     unsigned query_interval_time = 125; // QQIC: time between queries | default = 125 (seconds)
     unsigned robustness_variable = 2; // robustness from system | default = 2 (amount of joins/leaves send)
