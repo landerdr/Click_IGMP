b090057387f414ed101da48b6fdfc5088c78e768
LanderDeRoeck <Lander.DeRoeck@student.uantwerpen.be>
2020-01-06 15:57:41 +0100

    Hopefully working MRT

--- test_files/Scenario.sh
+++ test_files/Scenario.sh
@@ -2,41 +2,41 @@
 
 
 telnet localhost 10003 <<EOF
-write client21/rs.join 224.4.4.4
+write client21/igmp.join 224.4.4.4
 
 EOF
 sleep 10
 telnet localhost 10005 <<EOF
-write client31/rs.join 224.4.4.4
+write client31/igmp.join 224.4.4.4
 
 EOF
 sleep 10
 telnet localhost 10006 <<EOF
-write client32/rs.join 224.4.4.4
+write client32/igmp.join 224.4.4.4
 
 EOF
 sleep 10
 telnet localhost 10003 <<EOF
 
-write client21/rs.leave 224.4.4.4
+write client21/igmp.leave 224.4.4.4
 EOF
 sleep 10
 telnet localhost 10006 <<EOF
-write client32/rs.leave 224.4.4.4
+write client32/igmp.leave 224.4.4.4
 
 EOF
 sleep 10
 telnet localhost 10004 <<EOF
-write client22/rs.join 224.4.4.4
+write client22/igmp.join 224.4.4.4
 
 EOF
 sleep 10
 telnet localhost 10005 <<EOF
-write client31/rs.leave 224.4.4.4
+write client31/igmp.leave 224.4.4.4
 
 EOF
 sleep 10
 telnet localhost 10004 <<EOF
-write client22/rs.leave 224.4.4.4
+write client22/igmp.leave 224.4.4.4
 
 EOF
